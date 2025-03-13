0d007d344108ecf798276c44639daf9b184d74bb
Gabriel <frtzg16@gmail.com>
2025-03-13 17:09:02 -0300

    fix: resolve conflitos de merge e atualiza dependências

--- src/index.tsx
+++ src/index.tsx
@@ -1,33 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import './styles/global.css';
 import App from './App';
-=======
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
->>>>>>> 1d23411a394b9f0a1ec6231b6970d7858e52a75c
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-<<<<<<< HEAD
 
-=======
->>>>>>> 1d23411a394b9f0a1ec6231b6970d7858e52a75c
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-<<<<<<< HEAD
-=======
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
->>>>>>> 1d23411a394b9f0a1ec6231b6970d7858e52a75c
